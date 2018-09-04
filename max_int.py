f214b17be58c063ff5da0025151f18864f398e4b
sjunia <sigrunjunia@gmail.com>
2018-09-04 11:01:00 +0000

    Changes made

---
+++ max_int.py
@@ -0,0 +1,9 @@
+
+num_int = int(input("Input a number: "))    # Do not change this line
+max_int = 0
+
+if (num_int < 0):
+    print("your number is no longer a positive")
+
+else:
+    continue
--- maximum_number.py
+++
@@ -1,6 +0,0 @@
-
-num_int = int(input("Input a number: "))    # Do not change this line
-#
-while 
-
-print("The maximum is", max_int)    # Do not change this line
